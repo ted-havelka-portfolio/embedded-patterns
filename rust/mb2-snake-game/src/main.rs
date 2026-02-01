59a1eca832753690e852b590532355508f34d46b
Theodore Havelka <tedhavelka66@gmail.com>
2026-02-01 21:42:29 +0000

    apps: add non-blocking, greyscale display to Snake Game.  Note however . . .
    that LED array rendering is broken.  The entire row or column containing the
    head of the snake flickers, even though the snake begins with a length of two.
    Make the game un-playable.

--- rust/mb2-snake-game/src/main.rs
+++ rust/mb2-snake-game/src/main.rs
@@ -6,61 +6,63 @@
 
 mod game;
 mod control;
+mod display;
 
 use cortex_m_rt::entry;
 use microbit::{
-   Board,
-   // hal::{prelude::*, Rng, Timer},
-   // hal::{Rng, Timer},
-   hal::{
-       prelude::*,
-       Rng,
-       timer::Timer
-   },
-   display::blocking::Display
+    Board,
+    hal::{
+        prelude::*,
+        Rng,
+        timer::Timer
+    },
+    // display::blocking::Display
+    display::nonblocking::{BitImage, GreyscaleImage}
 };
 
 use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
 
-use crate::game::{Game, GameStatus, Turn};
-// use crate::game::{Game, GameStatus};
 use crate::control::{init_buttons, get_turn};
+use crate::display::{clear_display, display_image, init_display};
+use crate::game::{Game, GameStatus, Turn};
 
 #[entry]
 fn main() -> ! {
-   rtt_init_print!();
-   let board = Board::take().unwrap();
-   let mut timer = Timer::new(board.TIMER0);
-   let mut rng = Rng::new(board.RNG);
-   let mut game = Game::new(rng.random_u32());
-   let mut display = Display::new(board.display_pins);
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut rng = Rng::new(board.RNG);
+    let mut game = Game::new(rng.random_u32());
+    // let mut display = Display::new(board.display_pins);
 
-   init_buttons(board.GPIOTE, board.buttons);
+    init_buttons(board.GPIOTE, board.buttons);
+    init_display(board.TIMER1, board.display_pins);
 
-   loop {
-      loop {  // Game loop
-         let image = game.game_matrix(9, 9, 9);
-         // The brightness values are meaningless at the moment as we haven't yet
-         // implemented a display capable of displaying different brightnesses
-         display.show(&mut timer, image, game.step_len_ms());
-         match game.status {
-            // GameStatus::Ongoing => game.step(Turn::None), // Placeholder as we
-                                                          // haven't implemented
-                                                          // controls yet
-            GameStatus::Ongoing => game.step(get_turn(true)),
-            _ => {
-               for _ in 0..3 {
-                  display.clear();
-                  timer.delay_ms(200u32);
-                  display.show(&mut timer, image, 200);
-               }
-               display.clear();
-               display.show(&mut timer, game.score_matrix(), 1000);
-               break
+    loop {
+        loop {  // Game loop
+            // Game matrix parameters are brightness for snake head, tail and food
+            let image = GreyscaleImage::new(&game.game_matrix(9, 9, 9));
+            // display.show(&mut timer, image, game.step_len_ms());
+            display_image(&image);
+            match game.status {
+                GameStatus::Ongoing => game.step(get_turn(true)),
+                _ => {
+                    for _ in 0..3 {
+                        // display.clear();
+                        clear_display();
+                        timer.delay_ms(200u32);
+                        // display.show(&mut timer, image, 200);
+                        display_image(&image);
+                    }
+                    // display.clear();
+                    // display.show(&mut timer, game.score_matrix(), 1000);
+                    clear_display();
+                    display_image(&BitImage::new(&game.score_matrix()));
+                    break
+                }
             }
-         }
-      }
-      game.reset();
-   }
+        }
+        game.reset();
+    }
 }
